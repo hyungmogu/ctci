246b7d9a76ce4374e3f09a8be0c41f3d20b05177
Hyungmo Gu <guhyungm7@gmail.com>
2018-07-18 22:19:02 -0700

    solve string compression problem (Q1.6)

--- ctci/array_and_strings/string_compression/string_compression.py
+++ ctci/array_and_strings/string_compression/string_compression.py
@@ -1,38 +1,46 @@
 def compress_string(letters):
     compressed_str = ''
     current_cnt = 1
-    current_char = letters[0]
     letters_arr = [x for x in letters]
+    current_char = letters_arr[0]
     letters_size = len(letters_arr)
-    print("size of arr: " + str(letters_size))
 
+    # Edge case where letter size is 1
     if (letters_size == 1):
         return letters
 
-    for i in range(1, letters_size):
-        if (current_char == letters_arr[i]):
+    if (letters_size == 2):
+        if (current_char == letters_arr[letters_size-1]):
             current_cnt += 1
-
-        if (i == letters_size - 1 and current_cnt == 1):
-            compressed_str += current_char
-            break
-        elif (i == letters_size - 1 and current_cnt > 1):
             compressed_str += current_char + str(current_cnt)
-            break
+        else:
+            compressed_str += current_char + letters_arr[letters_size-1]
 
-        if (current_char != letters_arr[i] and current_cnt == 1):
-            compressed_str += current_char
+    if (letters_size > 2):
+        # Edge case where letter size is not 1
+        for i in range(1, letters_size):
+            # case where i is greater than 0
+            # case where the ith letter is equal to current char
+            if (current_char == letters_arr[i]):
+                current_cnt += 1
+                continue
+
+            # case where the ith letter is not equal to current char
+            if (current_char != letters_arr[i] and current_cnt == 1):
+                compressed_str += current_char
+
+            elif (current_char != letters_arr[i] and current_cnt > 1):
+                compressed_str += current_char + str(current_cnt)
 
             current_char = letters_arr[i]
             current_cnt = 1
 
-        elif (current_char != letters_arr[i] and current_cnt > 1):
+        # address the last letter in string
+        if (current_cnt > 1):
             compressed_str += current_char + str(current_cnt)
+        else:
+            compressed_str += current_char
 
-            current_char = letters_arr[i]
-            current_cnt = 1
-
-    print('compressed_str' + compressed_str)
     if (len(compressed_str) == letters_size):
         return letters
     else:
